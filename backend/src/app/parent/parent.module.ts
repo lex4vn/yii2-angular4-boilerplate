ad9de635bc749f12eb5d2226f07d1c8c00bad176
Lex <lex4vn@gmail.com>
2018-03-24 00:42:11 +0700

    update clazz

--- backend/src/app/parent/parent.module.ts
+++ backend/src/app/parent/parent.module.ts
@@ -6,13 +6,11 @@ import {ParentListComponent} from './parent-list.component';
 import {ParentFormComponent} from './parent-form.component';
 import {ParentRoutingModule} from './parent-routing.module';
 
-import { ImageUploadModule } from "angular2-image-upload";
 @NgModule({
     imports: [
         CommonModule,
         SharedModule,
         ParentRoutingModule,
-		ImageUploadModule.forRoot(),
     ],
     declarations: [
         ParentListComponent,
